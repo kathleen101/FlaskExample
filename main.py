ee495dda403b6b22bb5656dc4bb217cc47de4cb1
kathleen101 <81065967+kathleen101@users.noreply.github.com>
2022-07-23 23:20:58 -0700

    Corrected error and continuing into calculator.

--- main.py
+++ main.py
@@ -2,20 +2,38 @@ import os
 import base64
 from urllib import request
 
-from flask import Flask, render_template
+from flask import Flask, request, render_template, session
+
+from model import SavedTotal
 
 app = Flask(__name__)
+app.secret_key = b'&\x81\xed\t\sbd\scd\se2\s90B\x1a\xf0\xba\x8e*\xa7m9\x8az\xcehtD'
 
 
 @app.route('/add', methods=['GET', 'POST'])
 def add():
-    # if request.method == 'POST':
-    #     number = int(request.form['number'])
+    if 'total' not in session:
+        session['total'] = 0
 
-    return render_template('add.jinja2')
+    if request.method == 'POST':
+        number = int(request.form['number'])
+        session['total'] += number
+
+    return render_template('add.jinja2', session=session)
+
+
+@app.route('/save', methods=['POST'])
+def save():
+    total = session.get('total', 0)
+    code = base64.b32encode(os.urandom(8)).decode().strip("=")
+
+    saved_total = SavedTotal(value=total, code=code)
+    saved_total.save()
+
+    return render_template('save.jinja2', code=code)
 
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 50000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
 
